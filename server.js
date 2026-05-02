5757042ae8e01619f13a0335e2ace1428d03a20a
marinahevel24-hue <marinahevel24@gmail.com>
2026-05-02 20:26:59 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const crypto = require("crypto");
 
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 function base64(str) {
   return Buffer.from(str).toString("base64");
 }
@@ -17,7 +14,13 @@ function sign(data) {
     .digest("base64");
 }
 
-function createLiqPayForm(amount) {
+app.get("/", (req, res) => {
+  res.send("Server is working");
+});
+
+app.get("/pay", (req, res) => {
+  const amount = req.query.amount || 100;
+
   const json = {
     public_key: process.env.LIQPAY_PUBLIC,
     version: 3,
@@ -32,24 +35,17 @@ function createLiqPayForm(amount) {
   const data = base64(JSON.stringify(json));
   const signature = sign(data);
 
-  return `
-    <form method="POST" action="https://www.liqpay.ua/api/3/checkout">
+  res.send(`
+    <form id="liqpay" method="POST" action="https://www.liqpay.ua/api/3/checkout">
       <input type="hidden" name="data" value="${data}" />
       <input type="hidden" name="signature" value="${signature}" />
-      <button type="submit">Оплатить ${amount} грн</button>
     </form>
-  `;
-}
-
-// проверка сервера
-app.get("/", (req, res) => {
-  res.send("Server is working");
+    <script>
+      document.getElementById("liqpay").submit();
+    </script>
+  `);
 });
 
-// ОПЛАТА
-app.get("/pay", (req, res) => {
-  const amount = req.query.amount || 100;
-  res.send(createLiqPayForm(amount));
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
 });
-
-app.listen(process.env.PORT || 3000);
