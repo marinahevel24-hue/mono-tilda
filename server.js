83980f19d1f2a89a4fa3de4c272aab1fbfaf16c1
marinahevel24-hue <marinahevel24@gmail.com>
2026-05-02 19:20:34 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from "express";
 import crypto from "crypto";
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,16 +17,14 @@ function sign(data) {
     .digest("base64");
 }
 
-app.post("/create-payment", (req, res) => {
-  const amount = Number(req.body.amount || 1);
-
+function createLiqPayForm(amount) {
   const json = {
     public_key: process.env.LIQPAY_PUBLIC,
     version: 7,
     action: "pay",
-    amount: amount,
+    amount: Number(amount),
     currency: "UAH",
-    description: "Оплата замовлення",
+    description: "Оплата замовлення Secret Store",
     order_id: "order_" + Date.now(),
     result_url: "https://secretstore.com.ua"
   };
@@ -33,13 +32,27 @@ app.post("/create-payment", (req, res) => {
   const data = base64(JSON.stringify(json));
   const signature = sign(data);
 
-  res.send(`
+  return `
     <form id="liqpay" method="POST" action="https://www.liqpay.ua/api/3/checkout">
       <input type="hidden" name="data" value="${data}" />
       <input type="hidden" name="signature" value="${signature}" />
     </form>
     <script>document.getElementById("liqpay").submit();</script>
-  `);
+  `;
+}
+
+app.get("/", (req, res) => {
+  res.send("Server is working");
+});
+
+app.get("/pay", (req, res) => {
+  const amount = req.query.amount || 1;
+  res.send(createLiqPayForm(amount));
+});
+
+app.post("/create-payment", (req, res) => {
+  const amount = req.body.amount  req.body.payment?.amount  1;
+  res.send(createLiqPayForm(amount));
 });
 
 app.listen(process.env.PORT || 3000);
